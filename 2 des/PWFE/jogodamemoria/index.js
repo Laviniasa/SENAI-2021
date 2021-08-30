2a46545e67b166747572fd075f9e2a6391200bca
Lavínia Sá <82878705+Laviniasa@users.noreply.github.com>
2021-08-30 11:20:37 -0300

    jogo da memória js

---
+++ 2 des/PWFE/jogodamemoria/index.js
@@ -0,0 +1,21 @@
+function virarCarta(qual){
+    console.log ("clicou em "+qual.id)
+    let carta = document.getElementById(qual.id)
+    carta.setAttribute("style","background-image: url('./assets/"+cardObjt.bt1+"');")
+
+
+var friends = ["friends2.jpg","friends3.jpg","friends4.jpg","friends5.jpg","friends6.PNG"]
+var cardObjt = {
+    "nome":"bt1",
+    "frente":friends[Math.floor(Math.random() * 8 )]
+    card.verso = "friendscapa.jpg"
+    card.setAttribute(
+        "style",
+        "backgroud-image: url('./assets/"+card.frente+"');"
+    
+    )
+}
+
+
+
+
