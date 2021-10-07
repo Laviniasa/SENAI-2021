efe1716486a585b93f757b0acb50690242bb1b4d
Lavínia Sá <82878705+Laviniasa@users.noreply.github.com>
2021-10-07 11:24:33 -0300

    deliverysql

--- 2 des/PWBE/aula3009/deliveryapi/src/main/java/domains/Cliente.java
+++ 2 des/PWBE/aula3009/deliveryapi/src/main/java/domains/Cliente.java
@@ -1,5 +1,13 @@
 package domains;
 
 public class Cliente {
+	public Cliente() {
+	}
+	private int idCliente;
+	private String nomeCompleto;
+	private String cpf;
+	private String telefone;
+	private String endereco;
 
+	public Cliente
 }
--- 2 des/PWBE/aula3009/deliveryapi/src/main/java/domains/Item.java
+++ 2 des/PWBE/aula3009/deliveryapi/src/main/java/domains/Item.java
@@ -1,5 +1,10 @@
 package domains;
 
 public class Item {
-
-}
+	private Pedido pedido;
+	private Produto produto;
+	private int quanntidade;
+	
+	
+	public Item() {
+	}
