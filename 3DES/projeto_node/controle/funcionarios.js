b4cb0ade22ce5587e5e94aed8f0a8824551e2007
Lavínia Sá <82878705+Laviniasa@users.noreply.github.com>
2021-12-03 11:13:58 -0300

    Add files via upload

--- 3DES/projeto_node/controle/funcionarios.js
+++ 3DES/projeto_node/controle/funcionarios.js
@@ -1,19 +1,86 @@
-const { con } = require("./bdconnect.js")
-const get = (req, ress)=>{
+//Importação da conexão do banco de dados
+const {
+    con
+} = require('./bdconnect.js')
+const modelo = require('../modelo/funcionario.js')
+
+//Função que faz a requisição do banco de dados fazendo uso da conexão
+const pegar = (req, res) => {
     let string = 'select * from funcionarios'
-    con.query(string, (err, result)=>{
-        req.json(result)
-    })  
+    con.query(string, (err, result) => {
+        res.json(result)
+    })
+}
+
+const pegarid = (req, res) => {
+    let string = `select * from funcionarios where matricula = ${req.params.matricula}`
+    con.query(string, (err, result) => {
+        if (result == "") {
+            res.status(404).end()
+        } else {
+            res.json(result)
+        }
+    })
+}
+
+const calcirrf = (req, res) => {
+    let string = 'select * from funcionarios'
+    con.query(string, (err, result) => {
+        let array = []
+        result.forEach(e => {
+            array.push(modelo.calcIRRF(e))
+        })
+        res.json(array)
+    })
 }
 
 
-
-
-const post = (req, ress)=>{
-    res.json(req.body)
+const enviar = (req, res) => {
+    let nome_completo = "\"" + req.body.nome_completo + "\"";
+    let data_desligamento = "\"" + req.body.data_desligamento + "\"";
+    let ultimo_salario = req.body.ultimo_salario;
+    let string = `insert into funcionarios(nome_completo, data_desligamento, ultimo_salario) values (${nome_completo}, ${data_desligamento}, ${ultimo_salario})`;
+    con.query(string, (err, result) => {
+        if (result.affectedRows == 1) {
+            res.json(modelo.reformJson(req.body, result.insertId));
+        } else {
+            res.status(400).end();
+        }
+    })
 }
 
+const atualizar = (req, res) => {
+    let matricula = req.body.matricula;
+    let nome_completo = "\"" + req.body.nome_completo + "\"";
+    let data_desligamento = "\"" + req.body.data_desligamento + "\"";
+    let ultimo_salario = req.body.ultimo_salario;
+    let string = `update funcionarios set nome_completo = ${nome_completo}, data_desligamento = ${data_desligamento}, ultimo_salario = ${ultimo_salario} where matricula = ${matricula}`;
+    con.query(string, (err, result) => {
+        if (result.changedRows == 1) {
+            res.json(req.body)
+        } else {
+            res.status(404).end()
+        }
+    })
+}
+
+const apagar = (req, res) => {
+    let string = `delete from funcionarios where matricula = ${req.params.matricula}`
+    con.query(string, (err, result) => {
+        if (result.affectedRows == 1) {
+            res.status(410).end()
+        } else {
+            res.status(404).end()
+        }
+    })
+}
+
+//Exportação da função
 module.exports = {
-    get,
-    post
+    pegar,
+    enviar,
+    atualizar,
+    apagar,
+    pegarid,
+    calcirrf
 }
--- 3DES/projeto_node/rotas/funcionarios.js
+++ 3DES/projeto_node/rotas/funcionarios.js
@@ -1,7 +1,18 @@
+//Importação do express
 const express = require('express');
+//Faz o gerenciamento dos metodos HTTP
 const rotas = express.Router();
+
+//Importação do controle dos funcionarios
 const controle = require('../controle/funcionarios.js');
 
+//Metodo HTTP com link pós DNS que executa o controle
 rotas.get('/api', controle.pegar);
-rotas.post('api/post', controle.enviar);
+rotas.get('/api/:matricula', controle.pegarid);
+rotas.get('/api/calc/all', controle.calcirrf);
+rotas.post('/api/post', controle.enviar);
+rotas.put('/api/put', controle.atualizar);
+rotas.delete('/api/delete/:matricula', controle.apagar);
+
+//Exporta as rotas para o arquivo principal
 module.exports = rotas;
