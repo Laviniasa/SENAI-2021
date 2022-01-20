a63bbc18e589eceb29df0297c8caff8eceec3892
Lavínia Sá <82878705+laviniasa@users.noreply.github.com>
2022-01-20 11:29:46 -0300

    Add files via upload

---
+++ 3DES/proj_blitz/backend/src/model/usuario.js
@@ -0,0 +1,30 @@
+const {Model, DataTypes} = require('sequelize');
+
+class Usuario extends Model {
+    static init(datacon) {
+        super.init(
+            {
+                email:{
+                    type: DataTypes.STRING(100),
+                    allowNull: false,
+                },
+                senha:{
+                    type: DataTypes.STRING(150),
+                    allowNull: false,
+                },
+                foto: {
+                    type: DataTypes.STRING(5000),
+                    allowNull: false,
+                }
+            },
+            {
+                sequelize: datacon,
+                tableName: 'usuarios',
+                modelName: 'usuario',
+            }
+        );
+    }
+}
+
+
+module.exports = Usuario;
